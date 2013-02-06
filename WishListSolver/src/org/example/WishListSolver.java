08c1d4c8bd5d541623af33ff03c43e53a67f5161
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2013-02-06 17:39:21 +0900

    real items

--- WishListSolver/src/org/example/WishListSolver.java
+++ WishListSolver/src/org/example/WishListSolver.java
@@ -17,15 +17,19 @@ public class WishListSolver {
 
         // setup
         WishListSolution unsolvedSolution = new WishListSolution();
-        Item item1 = new Item("aaa", 1000);
-        Item item2 = new Item("bbb", 8000);
-        Item item3 = new Item("ccc", 5000);
-        Item item4 = new Item("ddd", 15000);
+        Item item1 = new Item("北欧風フリース袖付きベスト 80cm", 1050);
+        Item item2 = new Item("おかゆこがま 200ml", 1015);
+        Item item3 = new Item("トロンプルイユT風ボディ/70", 3045);
+        Item item4 = new Item("ベビーソファ 専用腰ベルト入り", 3670);
+        Item item5 = new Item("スヌーピーストローマグ", 1260);
+        Item item6 = new Item("6重ガーゼケット トドラーケット", 6300);
         List<Item> itemList = new ArrayList<Item>();
         itemList.add(item1);
         itemList.add(item2);
         itemList.add(item3);
         itemList.add(item4);
+        itemList.add(item5);
+        itemList.add(item6);
         unsolvedSolution.setItemList(itemList);
         Bucket cart = new Bucket(true);
         Bucket dummy = new Bucket(false);
@@ -40,10 +44,14 @@ public class WishListSolver {
         WishListSolution solvedSolution = (WishListSolution) solver.getBestSolution();
 
         // Display the result
+        System.out.println("============= Best Solution =============");
+        int total = 0;
         for (Item item : solvedSolution.getItemList()) {
             if (item.isInCart() == true) {
                 System.out.println(item.getName() + " : price = " + item.getPrice());
+                total += item.getPrice();
             }
         }
+        System.out.println("Total = " + total);
     }
 }
