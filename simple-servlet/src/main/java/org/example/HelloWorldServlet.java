fd1ec22c086e85a4bce0a6cf6ca4c86b95ff0a6e
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2014-07-28 11:33:01 +0900

    InetAddress test

--- simple-servlet/src/main/java/org/example/HelloWorldServlet.java
+++ simple-servlet/src/main/java/org/example/HelloWorldServlet.java
@@ -1,7 +1,13 @@
 package org.example;
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,6 +25,15 @@ public class HelloWorldServlet extends HttpServlet {
    @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+       InetAddress localHost = InetAddress.getLocalHost();
+       System.out.println(localHost);
+       System.out.println(localHost.getHostAddress());
+       
+       System.out.println("req.getLocalAddr() = " + req.getLocalAddr());
+       
+       getAppStatus("http://localhost:8080/dashbuilder/workspace/jbpm-dashboard");
+       
+       
       PrintWriter writer = resp.getWriter();
       writer.println(PAGE_HEADER);
       writer.println("<h1>Hello, world!</h1>");
@@ -26,4 +41,59 @@ public class HelloWorldServlet extends HttpServlet {
       writer.close();
    }
 
+   public void getAppStatus(String theUrl) {
+       // Get a list of the urls to check for the given url
+       List<String> urls = explodeUrl(theUrl);
+       Exception exc = null;
+       for (String anUrl : urls) {
+           System.out.println("anUrl = " + anUrl);
+           try {
+               // Check whether the service is available
+               int status = pingUrl(anUrl);
+               System.out.println(status);
+               return;
+           } catch (Exception e){
+               System.out.println(e);
+               exc = e;
+           }
+       }
+       if (exc != null) exc.printStackTrace();
+   }
+
+   protected int pingUrl(String anUrl) throws Exception {
+       URL url = new URL(anUrl);
+       HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+       return urlConnection.getResponseCode();
+   }
+
+   protected List<String> explodeUrl(String anUrl) {
+       List<String> results = new ArrayList<String>();
+
+       // Add the target URL
+       results.add(anUrl);
+
+       // Add the "localhost" version
+       try {
+           String host = new URL(anUrl).getHost();
+           String localHost = InetAddress.getLocalHost().getHostAddress();
+           if (!host.equals(localHost)) {
+               results.add(anUrl.replace(host, localHost));
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       
+       // Add jboss.bind.address
+       try {
+           String host = new URL(anUrl).getHost();
+           String bindAddress = System.getProperty("jboss.bind.address");
+           if (bindAddress != null && !host.equals(bindAddress)) {
+               results.add(anUrl.replace(host, bindAddress));
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       
+       return results;
+   }
 }
