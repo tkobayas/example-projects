3bc8c14ddc915075235cf60af05bbe7caf150916
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2017-01-13 11:56:09 +0900

    for EAP 6.4

--- helloworld-rs/src/main/java/org/example/HelloWorld.java
+++ helloworld-rs/src/main/java/org/example/HelloWorld.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Produces;
 @Path("/")
 public class HelloWorld {
 
+    // curl http://localhost:8080/helloworld-rs-1.0.0/json
 	@GET
 	@Path("/json")
 	@Produces({ "application/json" })
@@ -14,6 +15,7 @@ public class HelloWorld {
 		return "{\"result\":\"Hello World!\"}";
 	}
 
+	// curl http://localhost:8080/helloworld-rs-1.0.0/xml
 	@GET
 	@Path("/xml")
 	@Produces({ "application/xml" })
