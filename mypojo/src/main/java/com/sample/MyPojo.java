ffe02dd2a947734f84c1de320e221ec674849f0a
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2017-02-13 11:34:25 +0900

    simplest mypojo

---
+++ mypojo/src/main/java/com/sample/MyPojo.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+public class MyPojo {
+
+    private String name;
+
+    public MyPojo() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
